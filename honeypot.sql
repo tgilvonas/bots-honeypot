52cdf9eddb4ccf29dff624a5af3db4c5922ef6a5
tgilvonas <tgilvonas@gmail.com>
2021-11-28 17:01:45 +0200

    Registration form has been finished

---
+++ honeypot.sql
@@ -0,0 +1,45 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               8.0.13 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table bots-honeypot.guestbook
+DROP TABLE IF EXISTS `guestbook`;
+CREATE TABLE IF NOT EXISTS `guestbook` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `testimonial` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table bots-honeypot.guestbook: ~0 rows (approximately)
+DELETE FROM `guestbook`;
+/*!40000 ALTER TABLE `guestbook` DISABLE KEYS */;
+/*!40000 ALTER TABLE `guestbook` ENABLE KEYS */;
+
+-- Dumping structure for table bots-honeypot.register
+DROP TABLE IF EXISTS `register`;
+CREATE TABLE IF NOT EXISTS `register` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table bots-honeypot.register: ~0 rows (approximately)
+DELETE FROM `register`;
+/*!40000 ALTER TABLE `register` DISABLE KEYS */;
+/*!40000 ALTER TABLE `register` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- post-actions/register.php
+++ post-actions/register.php
@@ -1,3 +1,33 @@
 <?php
 
+require '../config.php';
 
+/*
+echo '<pre>';
+var_dump($connection);
+echo '</pre>';
+*/
+
+// Create connection
+//$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($connection['db_host'], $connection['db_user'], $connection['db_password'], $connection['db_name']);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO register (name, email) VALUES (?, ?)");
+$stmt->bind_param("ss", $name, $email);
+
+// set parameters and execute
+$name = $_POST['name'];
+$email = $_POST['email'];
+$stmt->execute();
+
+$stmt->close();
+$conn->close();
+
+header('Location: /order-food-or-table.php');
+exit();
